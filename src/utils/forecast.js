5080348fffa3cec4e89a1d7b3d327e1cf1b3bd3f
Filipe Santos <48060475+fc-santos@users.noreply.github.com>
2021-07-03 13:00:52 -0400

    Add wind speed in forecast result

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location');
     } else {
-      callback(undefined, `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. There is a ${body.current.precip}% chance of rain.`);
+      callback(undefined, `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. There is a ${body.current.precip}% chance of rain. Wind speed is at ${body.current.wind_speed} kmph.`);
     }
   });
 };
